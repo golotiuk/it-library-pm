f315702a1c71a150899875aef069e4c77b8d0ae1
Taras <email@mail.com>
2020-03-06 14:18:46 +0200

    v2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/build'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/build', function(req, res) {
 
     // ejs render automatically looks in the views folder
     res.render('index');
